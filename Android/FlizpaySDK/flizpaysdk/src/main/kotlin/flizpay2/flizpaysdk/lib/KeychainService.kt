3d5ada6aa193e8d2bd5537378118359af97bf0d0
Thrashattack <henrique2092@gmail.com>
2025-03-10 18:17:09 +0100

    feat: implement basic keychain service

--- Android/FlizpaySDK/flizpaysdk/src/main/kotlin/flizpay2/flizpaysdk/lib/KeychainService.kt
+++ Android/FlizpaySDK/flizpaysdk/src/main/kotlin/flizpay2/flizpaysdk/lib/KeychainService.kt
@@ -1,1 +1,36 @@
-// TODO
+package flizpay2.flizpaysdk.lib
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+
+class KeychainService(context: Context, keychainAccessKey: String) {
+    private val sharedPreferences: SharedPreferences
+
+    init {
+        val masterKey = MasterKey.Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            keychainAccessKey,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+    fun storeCredentials(key: String, value: String) {
+        sharedPreferences.edit().putString(key, value).apply()
+    }
+
+    fun fetchCredentials(key: String): String? {
+        return sharedPreferences.getString(key, null)
+    }
+
+    fun clearCredentials(key: String) {
+        sharedPreferences.edit().remove(key).apply()
+    }
+}
